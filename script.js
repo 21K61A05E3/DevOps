1f88854901998c7a4738664569dfe9190624cb00
21K61A05E3 <krishan.sampara@sasi.ac.in>
2024-04-25 11:45:51 +0530

    Newly updated

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ function submitForm(event){
     var username= document.getElementById('username').value;
     var email=document.getElementById('email').value;
     var password= document.getElementById('password').value;
-    var phone=document.getElementById('phone').value;
     if(!validateUsername(username)||!validateEmail(email)||!validatePassword(password)){
         return;
     }
@@ -22,14 +21,6 @@ function validateEmail(email){
 function validatePassword(password){
     if(password.length<6){
         alert('Password must be atleast 6 characters long');
-        return false;
-    }
-    return true;
-}
-function validatePhone(phone){
-    const phoneRegex=/^\d{10}$/;
-    if(!phoneRegex.test(phone)){
-        alert('Invalid phone number. Please enter a 10-digit number.');
         return false;
     }
     return true;
